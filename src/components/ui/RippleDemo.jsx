8aff90af1f0a4b4a200ae6115780c27469c527ce
Saichandran28 <s.saichandran2002@gmail.com>
2025-01-20 19:12:34 +0530

    Mobile view Photo updated

--- src/components/ui/RippleDemo.jsx
+++ src/components/ui/RippleDemo.jsx
@@ -4,9 +4,9 @@ import logo from "../../assets/Logo.jpg"
 
 export function RippleDemo() {
   return (
-    <div className="relative flex h-[500px] w-[700px] rounded-full sm:h-[700px] sm:w-[700px] sm:rounded-none flex-col sm:flex-row items-center justify-center overflow-hidden">
+    <div className="relative flex h-[300px] w-[300px] mb-10 sm:mb-0 rounded-full sm:h-[700px] sm:w-[700px] sm:rounded-none flex-col sm:flex-row items-center justify-center overflow-hidden">
       <p className="z-50 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-white">
-        <img src={logo} className="h-80 w-80 md:h-96 md:w-96 object-cover object-top rounded-full" alt="" />
+        <img src={logo} className="h-64 w-64 md:h-96 md:w-96 object-cover object-top rounded-full" alt="" />
       </p>
       <Ripple />
     </div>
